ab12e008d4eea00f9874a891b68e8bde5a6a73c9
Fábio Olivei <ofabioolivei@gmail.com>
2024-03-01 15:19:50 -0300

    Add files via upload

---
+++ YouTube_Insights_( EDA )/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project analyzes YouTube's landscape, identifying popular channels, their categories, and global impact, emphasizing the correlation between channel engagement, creators' earnings, and the significance of data analytics in content optimization.',
+    author='Fabio Olivei',
+    license='MIT',
+)
