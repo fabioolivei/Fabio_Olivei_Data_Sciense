b235a403dd32b64f1d09e07d96fbc8a586c0c8fc
Fábio Olivei <ofabioolivei@gmail.com>
2024-03-02 19:50:43 -0300

    Add files via upload

---
+++ Top_100_Highest-Grossing_Movies/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The analysis aims to identify variables closely associated with movie box office revenue, compare revenues across different countries, genres, and time periods, discern trends over time, highlight top-ranking successful franchises, and use charts to easily gain insights into rankings over time.',
+    author='Fabio Olivei',
+    license='MIT',
+)
