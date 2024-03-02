92e13571968173ad8f9638a7b338a964c92faab4
Fábio Olivei <ofabioolivei@gmail.com>
2024-03-02 19:34:34 -0300

    Add files via upload

---
+++ Analyzing Airbnb Data (Lisbon)/docs/index.rst
@@ -0,0 +1,24 @@
+.. Analyzing Airbnb Data (Lisbon) documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Analyzing Airbnb Data (Lisbon) documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ Analyzing Airbnb Data (Lisbon)/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Based on the data and analyses conducted, we observe that the majority of properties in Lisbon listed on Airbnb are full houses or apartments available for rent. Moreover, there is also a significant number of private rooms available. We can also define the average of the highest and lowest values for the 10 neighborhoods.',
+    author='Fabio Olivei',
+    license='MIT',
+)
