731e48a042be4983edbb1d40bee8f0e322b65fac
Fábio Olivei <ofabioolivei@gmail.com>
2024-03-02 19:47:29 -0300

    Add files via upload

---
+++ ML Applications in Fetal Health Diagnostics/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The primary goal is to develop a Machine Learning (ML) model that accurately classifies fetal health into three categories: 'Normal', 'Suspect', and 'Pathological'. This model aims to support early detection and intervention strategies, enhancing prenatal care and improving pregnancy outcomes.',
+    author='Fabio Olivei',
+    license='MIT',
+)
