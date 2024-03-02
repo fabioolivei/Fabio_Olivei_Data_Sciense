6d15ff6831903912ea5d929956009b760c429e49
Fábio Olivei <ofabioolivei@gmail.com>
2024-03-02 20:02:24 -0300

    Add files via upload

---
+++ The Bar Chart Race as a Data Visualization Tool/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The bar chart race is a dynamic chart that shows the evolution of a value over time. It is a very effective way of visualizing data, as it allows users to see how values change over time in a quick and easy manner',
+    author='Fabio Olivei',
+    license='MIT',
+)
