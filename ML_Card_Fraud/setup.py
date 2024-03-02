f0cc9d08785792a38b516e8720affe4ce4dc9f04
Fábio Olivei <ofabioolivei@gmail.com>
2024-03-02 20:25:15 -0300

    Add files via upload

---
+++ ML_Card_Fraud/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementing our project's Machine Learning model, a company escalates its fraud detection efficiency',
+    author='Fabio Olivei',
+    license='MIT',
+)
