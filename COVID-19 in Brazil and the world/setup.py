8086b56f0485190b8923df87a9d5c43423f3162e
Fábio Olivei <ofabioolivei@gmail.com>
2024-03-02 09:05:57 -0300

    Add files via upload

---
+++ COVID-19 in Brazil and the world/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The aim of this analysis is to examine the current data on COVID-19, both in Brazil and worldwide, three years after the pandemic began. We will look into insights through a comprehensive statistical analysis of the disease's situation.',
+    author='Fabio Olivei',
+    license='MIT',
+)
