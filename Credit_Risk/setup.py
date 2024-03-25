b81bf4b11a585818e8040a8bc7f87005a70365c0
Fábio Olivei <ofabioolivei@gmail.com>
2024-03-25 14:07:25 -0300

    Add files via upload

---
+++ Credit_Risk/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Assessing the risk of customer default is crucial for financial institutions like Nubank, which uses AI and Data Science to develop models aiming to predict and minimize such risks. These models are essential in reducing financial losses by accurately identifying potential delinquents upon their first card application.',
+    author='Fabio Olivei',
+    license='MIT',
+)
