15a75ce8571bfb8c8ef50c16b41426953def2d39
Fábio Olivei <ofabioolivei@gmail.com>
2024-09-09 06:22:28 -0300

    Add files via upload

---
+++ Os_Insights_do_State_of_Data_Brasil_2023/docs/index.rst
@@ -0,0 +1,24 @@
+.. Os_Insights_do_State_of_Data_2023 documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Os_Insights_do_State_of_Data_2023 documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ Os_Insights_do_State_of_Data_Brasil_2023/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='O “State of Data 2023” é uma pesquisa anual conduzida pela Data Hackers, há maior comunidade de profissionais de dados do Brasil. O relatório tem como objetivo mapear o cenário do mercado de dados no Brasil, fornecendo insights sobre as tendências, desafios, salários, tecnologias, ferramentas e o perfil dos profissionais que atuam nas áreas de ciência de dados, engenharia de dados, análise de dados, e outras funções relacionadas.',
+    author='Fábio Olivei',
+    license='MIT',
+)
